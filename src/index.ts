9b8b2367052e64058bd8c9d93070233a39c41f81
Reyner Loza <reyner.loza@leniolabs.com>
2022-09-28 10:00:32 -0500

    fix: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import express from 'express'
 import { randomSleep } from '../utils'
+import cors from 'cors'
 import JsonResponse from '../utils/randomResponse.json'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors)
 
 const PORT = process.env.PORT || 3000
 
 app.get('/:id', async (req, res) => {
-  const id = req.params.id
+  const id = parseInt(req?.params?.id)
 
   console.time('init')
-  const sleepTime = await randomSleep()
+  let sleepTime
+  if (id === 100) {
+    sleepTime = await randomSleep()
+  }
   console.time('init')
   console.log('finishid sleep')
   // console.lo
@@ -20,7 +25,7 @@ app.get('/:id', async (req, res) => {
   const jsonResponse = JSON.parse(JSON.stringify(JsonResponse))
 
   jsonResponse.id = id
-  jsonResponse.sleepTime = sleepTime
+  jsonResponse.sleepTime = sleepTime || 0
 
   res.send(JSON.stringify(jsonResponse))
 })
