9c597aaba34c8c5a828ff001c59192164207e622
Reyner Loza <reyner.loza@leniolabs.com>
2022-09-28 10:04:38 -0500

    add cors

--- build/src/index.js
+++ build/src/index.js
@@ -14,20 +14,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const utils_1 = require("../utils");
+const cors_1 = __importDefault(require("cors"));
 const randomResponse_json_1 = __importDefault(require("../utils/randomResponse.json"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use(cors_1.default);
 const PORT = process.env.PORT || 3000;
 app.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const id = req.params.id;
+    var _a;
+    const id = parseInt((_a = req === null || req === void 0 ? void 0 : req.params) === null || _a === void 0 ? void 0 : _a.id);
     console.time('init');
-    const sleepTime = yield (0, utils_1.randomSleep)();
+    let sleepTime;
+    if (id === 100) {
+        sleepTime = yield (0, utils_1.randomSleep)();
+    }
     console.time('init');
     console.log('finishid sleep');
     // console.lo
     const jsonResponse = JSON.parse(JSON.stringify(randomResponse_json_1.default));
     jsonResponse.id = id;
-    jsonResponse.sleepTime = sleepTime;
+    jsonResponse.sleepTime = sleepTime || 0;
     res.send(JSON.stringify(jsonResponse));
 }));
 app.listen(PORT, () => {
